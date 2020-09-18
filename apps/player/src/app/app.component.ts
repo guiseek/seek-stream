f06d5d4fe1869f9d73bc098f80b61a33e9fff3ff
Guilherme Siquinelli (guiseek) <guiseek@gmail.com>
2020-09-17 21:19:53 -0300

    stream player

--- apps/player/src/app/app.component.ts
+++ apps/player/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'seek-sound-root',
+  selector: 'seek-stream-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
--- apps/player/src/app/app.module.ts
+++ apps/player/src/app/app.module.ts
@@ -1,11 +1,18 @@
+import { AppRoutingModule } from './app.routing';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
